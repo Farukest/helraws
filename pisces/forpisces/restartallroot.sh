106486c41944519f8530b25e26899dd6ea9d6267
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-12-22 13:21:50 +0300

    changes

--- pisces/forpisces/restartallroot.sh
+++ pisces/forpisces/restartallroot.sh
@@ -1,3 +1,9 @@
+echo "admin" | sudo -S sleep 1 && sudo su - root
+sleep 2
+
+docker stop miner && docker rm miner && docker image pull quay.io/team-helium/miner:miner-arm64_2022.03.23.1_GA && sudo docker run -d --init --ulimit nofile=64000:64000 --restart always --net host -e OTP_VERSION=23.3.4.7 -e REBAR3_VERSION=3.16.1 --name miner --mount type=bind,source=/home/pi/hnt/miner,target=/var/data --mount type=bind,source=/home/pi/hnt/miner/log,target=/var/log/miner --device /dev/i2c-0  --privileged -v /var/run/dbus:/var/run/dbus --mount type=bind,source=/home/pi/hnt/miner/configs/sys.config,target=/config/sys.config quay.io/team-helium/miner:miner-arm64_2022.03.23.1_GA
+
+
 echo 'START...'
 
 echo 'HERŞEY BAŞTAN KURULUYOR SAKİN OL :) ...'
